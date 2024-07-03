4f394149ad0377546825d54c7609fdf9a1616a28
sinchanaxworkz28 <sinchanaraj.xworkz@gmail.com>
2024-07-03 11:03:20 +0530

    new program to develop about heater

---
+++ Heater.java
@@ -0,0 +1,8 @@
+class Heater{     
+public static void main (String[] bajaj){
+	System.out.println("Powerful heat that fits everywhere: Dreo space heaters for indoor combines Hyperamics 1500W advanced PTC system with a new heat funnel design to deliver heat within seconds and enhance warm air circulation for up to 200% farther reach than traditional heaters.");
+	System.out.println("Unbreakable safety: Dreo goes beyond the unexpected to deliver peace of mind with every room heater. The innovative tilt-detection sensor improves accuracy and reliability for better tip-over protection, and built with V0 flame retardant materials, overheat protection, and safety plug, all certified by ETL.");
+	System.out.println("Ultimate temperature control: The enhanced NTC chipset brings more precision to turn your space heater with thermostat into a smart heating powerhouse, allowing you to set heat on a wider range from 41-95℉ with ±1℉ increments, saving you energy while keeping you comfortable.");
+	System.out.println("Cozy quietness: No more choosing between quiet or warmth, Dreo's heater for bedroom warms quietly thanks to its brushless DC motor with frictionless performance and a winglet fan design that reduces turbulence, reducing noise to only 34dB.");
+	}
+}
