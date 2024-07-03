7f069f143471a3b795fbde480e306915d8dcd452
sinchanaxworkz28 <sinchanaraj.xworkz@gmail.com>
2024-07-03 10:54:52 +0530

    basic java program

---
+++ CellPhone.java
@@ -0,0 +1,10 @@
+class CellPhone{
+
+     public static void main(String() motorola){
+
+        System.out.println("Cell Phone is used for Communication.");
+		System.out.println(" Cell Phone is essential thing in today's life");
+	
+	}
+
+}
